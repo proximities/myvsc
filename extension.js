73b2131bd9f5c986bf526722888ef264e480a118
Bill Gan <billgan.pblpbl@gmail.com>
2024-02-03 22:19:46 -0500

    .

--- extension.js
+++ extension.js
@@ -1,68 +1,126 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+const vscodeExtSelection = require('vscode-ext-selection');
+// const { selectWordAtCursorPosition } = require('selectWordAtCursorPosition');
+
+// import { selectWordAtCursorPosition } from "vscode-ext-selection";
+
+
+
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+
 	console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.addUsingStatement', () => {
-		// The code you place here will be executed every time your command is executed
+	vscode.commands.registerCommand('extension.addDefineStatement', () => {
+
 		const textEditor = vscode.window.activeTextEditor;
 		if (!textEditor) {
 			vscode.window.showInformationMessage('No active text editor');
 		}
 		else {
 			let selection = textEditor.selection;
-			// get selected text
-			let word = textEditor.document.getText(selection);
 
-			// if it starts with "D3D_", "DXGI_", or "D3D11_", then add a "using" statement at the top of the file
+			let word = "";
+			if (vscodeExtSelection.selectWordAtCursorPosition(textEditor)) {
+				word = textEditor.document.getText(textEditor.selection);
+			}
+			else {
+				vscode.window.showErrorMessage('could not select word');
+				return;
+			}
+
 			if (word.startsWith('D3D11_') || word.startsWith('DXGI_') || word.startsWith('D3D_')) {
 				let edit = new vscode.WorkspaceEdit();
-				// trim the left side up to the first _ i.e. D3D_... becomes ...
+
 				let short = word.substring(word.indexOf('_') + 1);
-				// insert the using statement at the top of the file
-				let usingStatement = `using ${short} = ${word};\n`;
-				// check if the file already has a using statement
-				// if not, then add it
-				if (textEditor.document.getText().indexOf(usingStatement) == -1) {
-					edit.insert(textEditor.document.uri, new vscode.Position(0, 0), usingStatement);
+				// also trim underscores
+				// make short PascalCase
+				// split short by underscore
+				// capitalize each word
+				// join the words
+				short = short.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
+				// short = short.replace(/_/g, '');
+
+				let defineStatement = `#define ${short} ${word}\n`;
+
+
+				if (textEditor.document.getText().indexOf(defineStatement) == -1) {
+					edit.insert(textEditor.document.uri, new vscode.Position(0, 0), defineStatement);
 					vscode.workspace.applyEdit(edit);
 				}
 			}
 			else if (word.startsWith('ID3D') || word.startsWith('IDXGI') || word.startsWith('ID3D11')) {
-				// same thing as before, but add using for the pointer type 
+
 				let edit = new vscode.WorkspaceEdit();
-				// remove id3d or idxgi or id3d11
-				// string replace
+
+
 				let short = word.replace('ID3D11', '').replace('IDXGI', '').replace('ID3D', '');
-				let usingStatement = `using ${short} = ${word} *;\n`;
-				if (textEditor.document.getText().indexOf(usingStatement) == -1) {
-					edit.insert(textEditor.document.uri, new vscode.Position(0, 0), usingStatement);
+				let defineStatement = `#define ${short} ${word} *\n`;
+				if (textEditor.document.getText().indexOf(defineStatement) == -1) {
+					edit.insert(textEditor.document.uri, new vscode.Position(0, 0), defineStatement);
 					vscode.workspace.applyEdit(edit);
 				}
+			}
+			// focus on the first line
+			textEditor.selection = new vscode.Selection(0, 0, 0, 0);
+			// also scroll to the first line
+			textEditor.revealRange(new vscode.Range(0, 0, 0, 0));
+		}
+	});
+
+
+	vscode.commands.registerCommand('extension.removeBuildFolder', () => {
+
+
+		let workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+		let buildPaths = [path.join(workspacePath, 'build'), path.join(workspacePath, 'out')];
+		for (p of buildPaths) {
+			if (fs.existsSync(p)) {
+				fs.rmdirSync(p, { recursive: true });
 			}
 		}
 	});
 
-	context.subscriptions.push(disposable);
+
+	vscode.commands.registerCommand('extension.makeGameCodeReadable', () => {
+		const textEditor = vscode.window.activeTextEditor;
+		if (!textEditor) {
+			vscode.window.showInformationMessage('No active text editor');
+		}
+		else {
+			textEditor.edit(edit => {
+				let range = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(textEditor.document.lineCount, 0));
+				let text = textEditor.document.getText(range);
+
+
+				let patterns = [/D3D11_/g, /DXGI_/g, /D3D_/g, /ID3D11/g, /IDXGI/g, /ID3D/g, /D3D11/g, /DXGI/g, /D3D/g];
+
+
+				patterns.forEach(pattern => {
+					text = text.replace(pattern, '');
+				});
+
+				text = text.replace(/typedef struct/g, 'struct');
+				text = text.replace(/typedef enum/g, 'enum');
+				text = text.replace(/typedef union/g, 'union');
+
+				edit.replace(range, text);
+			});
+		}
+	});
 }
 
-// this method is called when your extension is deactivated
+
 function deactivate() { }
 
-// eslint-disable-next-line no-undef
+
 module.exports = {
 	activate,
 	deactivate
